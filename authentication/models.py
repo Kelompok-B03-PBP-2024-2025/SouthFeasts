388dfbedf337b8424fef0a7180241444a0033265
Aliya Zahira Nadra <158556910+lollyyy@users.noreply.github.com>
2024-10-20 21:14:01 +0700

    Menambahkan models untuk profile

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='auth_user')
+    username = models.CharField(max_length=255)
+    fullname= models.CharField(max_length=255)
+    country = models.CharField(max_length=255, blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    profile_picture = models.TextField( blank=True, null=True)
+
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        USER = 'USER', 'User'
+
+    role = models.CharField(
+        max_length=5,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+    
