d772dcb872f123b2dd2fe58949fa06a4a5de7524
yukaagnd <gnadeyuka@gmail.com>
2024-10-22 11:05:12 +0700

    add urls

--- southfeast/urls.py
+++ southfeast/urls.py
@@ -18,8 +18,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dashboard/', include('main.urls')),
-    path('auth/', include ('authentication.urls')),
-    
+    path("",include("main.urls"))
 ]
