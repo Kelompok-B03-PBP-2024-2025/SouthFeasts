2dbf1266a27a2c5b9ed800117845906f66633e11
Aliya Zahira Nadra <158556910+lollyyy@users.noreply.github.com>
2024-10-23 16:46:06 +0700

    bikin yg di wishlist tapi belom final okayy

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
+from . import views
 
 app_name = 'wishlist'
 
 urlpatterns = [
+    # Collection URLs
+    path('', views.collection_list, name='collection-list'),
+    path('collection/add/', views.collection_add, name='collection-add'),
+    path('collection/<int:collection_id>/', views.collection_detail, name='collection-detail'),
+    path('collection/<int:collection_id>/edit/', views.collection_edit, name='collection-edit'),
+    path('collection/<int:collection_id>/delete/', views.collection_delete, name='collection-delete'),
+    path('collection/<int:collection_id>/set-default/', views.collection_set_default, name='collection-set-default'),
+
+    # Item URLs
+    path('create/', views.add_to_wishlist_from_menu, name='create'),
+    path('item/add/<int:menu_item_id>/', views.item_add, name='item-add'),
+    path('item/<int:item_id>/remove/', views.item_remove, name='item-remove'),
+    path('item/<int:item_id>/move/<int:collection_id>/', views.item_move, name='item-move'),
 ]
