0859abe73eaf6e4003c9f3db5f87c29b3d0e7d49
Aliya Zahira Nadra <158556910+lollyyy@users.noreply.github.com>
2024-10-24 20:51:43 +0700

    progress wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -11,10 +11,21 @@ urlpatterns = [
     path('collection/<int:collection_id>/edit/', views.collection_edit, name='collection-edit'),
     path('collection/<int:collection_id>/delete/', views.collection_delete, name='collection-delete'),
     path('collection/<int:collection_id>/set-default/', views.collection_set_default, name='collection-set-default'),
+    path('get-collections/', views.get_collections, name='get-collections'),
+    path('collection/create-ajax/', 
+            views.create_collection_ajax, 
+            name='create-collection-ajax'),
 
     # Item URLs
     path('create/', views.add_to_wishlist_from_menu, name='create'),
     path('item/add/<int:menu_item_id>/', views.item_add, name='item-add'),
     path('item/<int:item_id>/remove/', views.item_remove, name='item-remove'),
     path('item/<int:item_id>/move/<int:collection_id>/', views.item_move, name='item-move'),
+    path('item/<int:item_id>/add-to/<int:collection_id>/', 
+         views.add_item_to_collection, 
+         name='add-item-to-collection'),
+    
+    path('collections/get/', views.get_collections, name='get-collections'),
+    path('collections/create/ajax/', views.create_collection_ajax, name='create-collection-ajax'),
+    path('items/<int:item_id>/add-to/<int:collection_id>/', views.add_item_to_collection, name='add-item-to-collection'),
 ]
