f2d120ee503bd2d1e14dcb073e45719263b5ad6a
Aliya Zahira Nadra <158556910+lollyyy@users.noreply.github.com>
2024-10-25 20:58:04 +0700

    progress wishlist

--- wishlist/forms.py
+++ wishlist/forms.py
@@ -4,7 +4,7 @@ from .models import WishlistCollection
 class WishlistCollectionForm(forms.ModelForm):
     class Meta:
         model = WishlistCollection
-        fields = ['name', 'description', 'is_default']
+        fields = ['name', 'description']
         widgets = {
             'name': forms.TextInput(
                 attrs={
@@ -18,19 +18,13 @@ class WishlistCollectionForm(forms.ModelForm):
                     'placeholder': 'Collection Description (Optional)',
                     'rows': 3
                 }
-            ),
-            'is_default': forms.CheckboxInput(
-                attrs={
-                    'class': 'h-4 w-4 text-black border-gray-300 rounded focus:ring-black'
-                }
             )
         }
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
-        self.fields['is_default'].label = "Set as default collection"
-
+        
     def clean_name(self):
         name = self.cleaned_data.get('name')
         if self.user:
@@ -40,7 +34,6 @@ class WishlistCollectionForm(forms.ModelForm):
                 name=name
             ).exists()
             
-            # If we're editing, exclude the current instance
             if self.instance.pk:
                 exists = exists and self.instance.name != name
                 
