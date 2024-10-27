92e6a63722a110ab0c262dd9f8adada302a914da
annisadwiyanti <dwiyantiannisa01@gmail.com>
2024-10-27 19:12:43 +0700

    ubah path navbar admin

--- forum/tests.py
+++ forum/tests.py
@@ -21,6 +21,16 @@ class ForumViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'culinary_insights.html')
 
+    def test_show_main_filter_public_articles(self):
+        response = self.client.get(reverse('forum:show_main') + '?filter=public_articles')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.article, response.context['articles'])
+
+    def test_show_main_filter_your_articles(self):
+        response = self.client.get(reverse('forum:show_main') + '?filter=your_articles')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.article, response.context['articles'])
+
     def test_add_article(self):
         response = self.client.post(reverse('forum:add_article'), {
             'title': 'New Article',
@@ -34,16 +44,20 @@ class ForumViewsTest(TestCase):
         response = self.client.post(reverse('forum:edit_article', args=[self.article.id]), {
             'title': 'Updated Article Title',
             'content': 'Updated content for the article.',
-        })
+        }, follow=True)
         self.assertEqual(response.status_code, 200)
         self.article.refresh_from_db()
         self.assertEqual(self.article.title, "Updated Article Title")
 
-    def test_delete_article_unauthorized(self):
-        new_user = User.objects.create_user(username='newuser', password='newpass')
-        self.client.login(username='newuser', password='newpass')
+    def test_delete_article_authorized(self):
         response = self.client.post(reverse('forum:delete_article', args=[self.article.id]))
-        self.assertEqual(response.status_code, 403)  # Expect unauthorized access
+        self.assertRedirects(response, reverse('forum:show_main'))
+        self.assertFalse(Article.objects.filter(id=self.article.id).exists())
+
+    def test_delete_article_authorized(self):
+        response = self.client.post(reverse('forum:delete_article', args=[self.article.id]))
+        self.assertRedirects(response, reverse('forum:show_main') + '#articles-section')
+        self.assertFalse(Article.objects.filter(id=self.article.id).exists())
 
     def test_add_question(self):
         response = self.client.post(reverse('forum:add_question'), {
@@ -53,43 +67,52 @@ class ForumViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(Question.objects.filter(title="New Question").exists())
 
-    def test_edit_article(self):
-        # Kirim POST request untuk mengedit artikel dengan `follow=True` agar redirect diikuti otomatis
-        response = self.client.post(reverse('forum:edit_article', args=[self.article.id]), {
-            'title': 'Updated Article Title',
-            'content': 'Updated content for the article.',
+    def test_edit_question(self):
+        response = self.client.post(reverse('forum:edit_question', args=[self.question.id]), {
+            'title': 'Updated Question Title',
+            'question': 'Updated content for the question.',
         }, follow=True)
-        
-        # Memastikan bahwa status kode akhir setelah mengikuti redirect adalah 200 (halaman berhasil ditampilkan)
         self.assertEqual(response.status_code, 200)
-        
-        # Memastikan data artikel sudah diperbarui
-        self.article.refresh_from_db()
-        self.assertEqual(self.article.title, "Updated Article Title")
+        self.question.refresh_from_db()
+        self.assertEqual(self.question.title, "Updated Question Title")
 
-    def test_delete_question(self):
+    def test_delete_question_authorized(self):
         response = self.client.post(reverse('forum:delete_question', args=[self.question.id]))
         self.assertEqual(response.status_code, 200)
         self.assertFalse(Question.objects.filter(id=self.question.id).exists())
 
-    def test_delete_comment_unauthorized(self):
-        # Create a new comment by the test user
-        comment = Comment.objects.create(article=self.article, user=self.user, content="Test comment")
-        
-        # Switch to a different user
+    def test_delete_question_unauthorized(self):
         new_user = User.objects.create_user(username='newuser', password='newpass')
         self.client.login(username='newuser', password='newpass')
-        
+        response = self.client.post(reverse('forum:delete_question', args=[self.question.id]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_delete_comment_unauthorized(self):
+        comment = Comment.objects.create(article=self.article, user=self.user, content="Test comment")
+        new_user = User.objects.create_user(username='newuser', password='newpass')
+        self.client.login(username='newuser', password='newpass')
         response = self.client.post(reverse('forum:delete_comment', args=[comment.id]))
-        self.assertEqual(response.status_code, 403)  # Unauthorized access
+        self.assertEqual(response.status_code, 403)
 
     def test_delete_answer_unauthorized(self):
-        # Create an answer by the test user
         answer = Answer.objects.create(question=self.question, user=self.user, content="Test answer")
-
-        # Switch to a different user
         new_user = User.objects.create_user(username='newuser', password='newpass')
         self.client.login(username='newuser', password='newpass')
-
         response = self.client.post(reverse('forum:delete_answer', args=[answer.id]))
-        self.assertEqual(response.status_code, 403)  # Unauthorized access
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_article_unauthenticated(self):
+        self.client.logout()
+        response = self.client.post(reverse('forum:add_article'), {
+            'title': 'Unauthenticated Article',
+            'content': 'This should not be added without login.',
+        })
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_add_question_unauthenticated(self):
+        self.client.logout()
+        response = self.client.post(reverse('forum:add_question'), {
+            'title': 'Unauthenticated Question',
+            'question': 'This should not be added without login.',
+        })
+        self.assertNotEqual(response.status_code, 200)
